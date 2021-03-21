ade5bde4e451446eefe7784c1fab938064039491
Peter Watts <pwattsmail@gmail.com>
2021-03-21 14:32:04 +1100

    basic ui

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -10,87 +9,80 @@ export default function Home() {
       </Head>
 
       <main>
-      <div class="p-3 text-right justify-end">
-            <button type="submit" class="inline-flex justify-center py-4 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Connect Wallet
-            </button>
-          </div>
-      <div class="flex flex-container justify-center pt-10">
-      <div class="grid grid-cols-1 md:grid-cols-2">
-        <div class="px-8">
-          <img src="https://thisartworkisalwaysonsale.com/static/media/TAIAOS4.3cd60b66.png"></img>
+        <div class="p-3 text-right justify-end">
+          <button type="submit" class="inline-flex justify-center py-4 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Connect Wallet
+          </button>
         </div>
-        <div class="px-8">
-          <div class="flex flex-col">
-            <div>To own this artwork, you must deposit what you think it is worth.</div>
-            <div>Anyone can buy it off you, by depositing a higher amount.</div>
-            <div>Your deposit earns 10.15% interest, split between you & the artist</div>
-          </div>
-          <div class="flex flex-row py-5 font-semibold">
-            <div class="flex flex-col pr-12 border-r border-gray-300">
-              <div>Current Price</div>
-              <div class="text-4xl py-4">0.123 ETH</div>
-              <div class="text-gray-400">$123.45</div>
+        <div class="flex flex-container justify-center pt-10 px-8">
+          <div class="grid grid-cols-1 md:grid-cols-2">
+            <div class="px-8 lg:pl-32">
+              <img src="/art.png" class="shadow border"></img>
             </div>
-            <div class="flex flex-col pl-12">
-              <div>Artist Yield</div>
-              <div class="text-4xl py-4">0.131982 ETH</div>
-              <div class="text-gray-400">Current Owner Yield: 0.012 ETH</div>
+            <div class="px-8">
+              <div class="flex flex-col">
+                <div>To own this artwork, you must <span class="italic">deposit what you think it is worth.</span></div>
+                <div><span class="italic">Anyone can buy it off you</span>, by depositing a higher amount.</div>
+                <div>Your deposit <span class="italic">earns interest</span>, split between you and the artist.</div>
+              </div>
+              <div class="flex flex-row py-10 font-semibold">
+                <div class="flex flex-col pr-12 border-r border-gray-300">
+                  <div>Current Price</div>
+                  <div class="text-4xl py-4">0.123 ETH</div>
+                  <div class="text-gray-400">$123.45</div>
+                </div>
+                <div class="flex flex-col pl-12">
+                  <div>Artist Yield</div>
+                  <div class="text-4xl py-4">0.131982 ETH</div>
+                  <div class="text-gray-400">Owner Yield: 0.081712 ETH</div>
+                </div>
+              </div>
+              <div class="rounded shadow border p-8">
+                <div class="pb-4 font-semibold">Purchase and Set New Price: </div>
+                <div class="flex flex-row items-center text-lg">
+                  <div>
+                    <input default="0.13" class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600" type="number" />
+                  </div>
+                  <div class="px-3">ETH</div>
+                  <button type="submit" class="inline-flex justify-center ml-5 py-4 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Buy
+                  </button>
+                </div>
+                <div class="py-3 text-left text-gray-400 text-sm">
+                   - 0.00615 ETH to artist (5% commission)<br />
+                   - 0.11685 ETH to current owner<br />
+                   - 0.07 ETH refundable deposit (Earns 5.23% APY) <br />
+                </div>
+              </div>
+
+                 <div class="py-12 my-12 border-t border-gray-300">
+                    <h2 class="text-3xl">About</h2>
+
+                    <div class="py-5">As NFTs proliferate, how can we ensure that creators capture appropriate value?</div>
+
+                    <div class="pb-5">This artwork is the result of exploring this question, and looking for alternative ownership models that could answer it. It attempts to improve upon two existing royalty mechanisms:</div>
+
+                    <div class="pb-5">1. Secondary Sales Commissions - While this is a huge step forward, the creator only earns when the item changes hands. How can we increase turnover, or alternatively generate revenue in the absence of sales?</div>
+
+                    <div class="pb-5">2. Harberger Taxes - This mechanism solves the above, by putting the item "always on sale", and charging the owner a tax. But such a mechanism can be unappealing to people who are accustomed to traditional property rights.</div>
+
+                    <div class="pb-5">"This Artwork Earns Yield" introduces two tweaks to the Harberger Taxes model:</div>
+                    <div class="pb-5">
+                      - A fixed, refundable deposit, rather than a fee pool that must be topped up<br />
+                      - Instead of taxing the owner, both the creator and owner earn yield from the deposit
+                    </div>
+                    <div class="pb-5">While such a mechanism will not be appropriate for all NFTs, the hope is that the simpler UX may reduce friction enough to make it useful in some applications. </div>
+
+                    <div class="pb-5">We are also excited to start a conversation about how Ethereum's composability can be leveraged to create novel solutions to this challenge that would not otherwise be possible.</div>
+
+                    <h2 class="text-3xl">Credit</h2>./ hg';hg'
+                </div>
             </div>
-          </div>
-          <div class="flex flex-row items-center">
-            <div>Your Price: </div>
-            <div class="px-3">
-              <input class="input border border-gray-400 appearance-none rounded w-full px-3 py-3 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600" type="number" />
-            </div>
-            <div>ETH</div>
-          </div>
-          <div class="py-3 text-left">
-            <button type="submit" class="inline-flex justify-center py-4 px-6 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Buy
-            </button>
           </div>
         </div>
-      </div>
-      </div>
-        {/* <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
+        
       </main>
     </div>
   )
